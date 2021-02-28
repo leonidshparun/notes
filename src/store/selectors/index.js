526dfd87f23918cbc0bc8e9dfb7e9e70e9d25c36
LEANID SHPARUN <leonidshparun@gmail.com>
2021-02-28 13:07:30 +0300

    fix: minor fixes

--- src/store/selectors/index.js
+++ src/store/selectors/index.js
@@ -1,12 +1,29 @@
 import { combinedFilter, combinedSort } from 'config/list.config';
 import { createSelector } from 'reselect';
 
+// data
 const selectNotesData = (state) => state.data.data;
-export const routeSelector = (state) => state.view.route;
 const sortOption = (state) => state.data.selection.sortOption;
 export const searchQuerySelector = (state) => state.data.selection.searchQuery;
 export const selectedTagsSelector = (state) => state.data.selection.tags;
+export const globalTagsSelector = (state) => state.data.globalTags;
+export const activeNoteIdSelector = (state) => state.data.activeNoteId;
+export const globadLoaderSelector = (state) => state.data.loading;
 
+// view
+export const routeSelector = (state) => state.view.route;
+export const modalStateSelector = (state) => state.view.modal;
+export const mediaTypeSelector = (state) => state.view.media;
+export const themeSelector = (state) => state.view.theme;
+export const isNavVisibleSelector = (state) => state.view.isNavigationVisible;
+export const isSidebarVisibleSelector = (state) => state.view.isSidebarVisible;
+export const isInfoVisibleSelector = (state) => state.view.isInfoVisible;
+
+// user
+export const userEmailSelector = (state) => state.user.email;
+export const isLoggedInSelector = (state) => state.user.isLoggedIn;
+
+// custom
 export const selectFiltredAndSortedNotesList = createSelector(
     selectNotesData,
     routeSelector,
@@ -25,31 +42,11 @@ export const selectNotesIdList = createSelector(
 );
 
 export const selectNoteById = (state, noteId) => selectNotesData(state)[noteId];
-export const activeNoteIdSelector = (state) => state.data.activeNoteId;
-
-export const globalTagsSelector = (state) => state.data.globalTags;
-
-export const isLoggedInSelector = (state) => state.user.isLoggedIn;
-export const isNavVisibleSelector = (state) => state.view.isNavigationVisible;
-export const isSidebarVisibleSelector = (state) => state.view.isSidebarVisible;
-export const isInfoVisibleSelector = (state) => state.view.isInfoVisible;
 
 export const noteTextSelector = (noteId) => (state) => selectNoteById(state, noteId).text;
 export const noteTagsSelector = (noteId) => (state) => selectNoteById(state, noteId).tags;
 export const notePinSelector = (noteId) => (state) =>
     selectNoteById(state, noteId).pinned;
 
-export const activeNoteDataSelector = (state) => {
-    const { activeNoteId } = state.data;
-    return selectNoteById(state, activeNoteId);
-};
-
-export const modalStateSelector = (state) => state.view.modal;
-
-export const mediaTypeSelector = (state) => state.view.media;
-
-export const userEmailSelector = (state) => state.user.email;
-
-export const themeSelector = (state) => state.view.theme;
-
-export const globadLoaderSelector = (state) => state.data.loading;
+export const activeNoteDataSelector = (state) =>
+    selectNoteById(state, state.data.activeNoteId);
